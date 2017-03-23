bf8a7197e7d14844345feb451f7423b20c3c377b
Ajay Kumar Choudhary <ajay@froiden.com>
2017-03-23 15:53:45 +0530

    Fix: laravel installer

--- src/Views/environment.blade.php
+++ src/Views/environment.blade.php
@@ -2,8 +2,20 @@
 
 @section('title', trans('installer_messages.environment.title'))
 @section('style')
-    <link href="{{ asset('installer/bootstrap/css/bootstrap.css') }}" rel="stylesheet"/>
     <link href="{{ asset('installer/froiden-helper/helper.css') }}" rel="stylesheet"/>
+    <style>
+        .form-control{
+            height: 14px;
+            width: 100%;
+        }
+        .has-error{
+            color: red;
+        }
+        .has-error input{
+            color: black;
+            border:1px solid red;
+        }
+    </style>
 @endsection
 @section('container')
     @if (count($errors) > 0)
@@ -20,55 +32,47 @@
         <p class="alert alert-danger">{{ session('message') }}</p>
     @endif
 
-    <form method="post" action="{{ route('LaravelInstaller::environmentSave') }}" id="login-form">
-    <div class="modal-body">
-        <div class="box-body">
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Hostname</label>
+    <form method="post" action="{{ route('LaravelInstaller::environmentSave') }}" id="env-form">
+        <div class="form-group">
+            <label class="col-sm-2 control-label">Hostname</label>
 
-                <div class="col-sm-10">
-                    <input type="text" name="hostname" class="form-control" >
-                </div>
+            <div class="col-sm-10">
+                <input type="text" name="hostname" class="form-control" >
             </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Username</label>
-                <div class="col-sm-10">
-                    <input type="text" name="username" class="form-control">
-                </div>
-            </div>
-
-
-            <div class="form-group">
-                <label  class="col-sm-2 control-label">Password</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" name="password">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Database</label>
-                <div class="col-sm-10">
-                    <input type="text" name="database" class="form-control">
-                </div>
-            </div>
-
         </div>
-    </div>
-    <div class="modal-footer">
-        <div class="buttons">
-            <button class="button" onclick="checkEnv();return false">
-                {{ trans('installer_messages.next') }}
-            </button>
+        <div class="form-group">
+            <label class="col-sm-2 control-label">Username</label>
+            <div class="col-sm-10">
+                <input type="text" name="username" class="form-control">
+            </div>
         </div>
-    </div>
-   </form>
+        <div class="form-group">
+            <label  class="col-sm-2 control-label">Password</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="password">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-2 control-label">Database</label>
+            <div class="col-sm-10">
+                <input type="text" name="database" class="form-control">
+            </div>
+        </div>
+        <div class="modal-footer">
+            <div class="buttons">
+                <button class="button" onclick="checkEnv();return false">
+                    {{ trans('installer_messages.next') }}
+                </button>
+            </div>
+        </div>
+    </form>
     <script>
         function checkEnv() {
             $.easyAjax({
                 url: "{!! route('LaravelInstaller::environmentSave') !!}",
                 type: "GET",
-                data: $("#login-form").serialize(),
-                container: "#login-form",
+                data: $("#env-form").serialize(),
+                container: "#env-form",
                 messagePosition: "inline"
             });
         }
@@ -76,7 +80,6 @@
 @stop
 @section('scripts')
     <script src="{{ asset('installer/js/jQuery-2.2.0.min.js') }}"></script>
-    <script src="{{ asset('installer/bootstrap/js/bootstrap.js') }}"></script>
     <script src="{{ asset('installer/froiden-helper/helper.js')}}"></script>
     <script>
         $.ajaxSetup({
